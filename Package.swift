61d956877c22079364dc905de627e1f883f3506e
Lexie T <110391186+LexiePlaysFast@users.noreply.github.com>
2024-06-30 19:55:22 +0000

    Migrates to new gh-action

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,6 +14,10 @@ let package = Package(
     ),
   ],
   dependencies: [
+    .package(
+      url: "https://github.com/swiftwasm/carton",
+      from: "1.0.0"
+    ),
     .package(
       url: "https://github.com/swiftwasm/JavaScriptKit.git",
       from: "0.19.0"
